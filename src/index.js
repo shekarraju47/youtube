eafebd10531771952519a5c99b9ee191ced9d239
shekarraju47 <145138176+shekarraju47@users.noreply.github.com>
2023-11-26 10:44:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='youtube-clone'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
